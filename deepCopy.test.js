dcdfe0bccb3a613d47feae50ad214230e99f413d
limhizy15 <limhizy15@ajou.ac.kr>
2023-09-21 09:56:40 +0900

    test: deepCopy 테스트 코드 추가

--- deepCopy.test.js
+++ deepCopy.test.js
@@ -1,15 +1,28 @@
 import { deepCopy } from './deepCopy.js';
 
 describe('deep copy', () => {
-  test('heeji', () => {
+  test('deep copy한 객체의 값은 original과 copy가 같다.', () => {
     const original = {
-      a: 1,
-      b: 2
+      a: [4, 5, 6, {g: 7}],
+      b: new Date(),
+      c: /hello/g
     }
 
     const copy = deepCopy(original);
 
     expect(original).toEqual(copy)
+    expect(original).not.toBe(copy)
+  }),
+
+  test('deep copy한 객체의 참조값은 original과 copy가 다르다.', () => {
+    const original = {
+      a: [4, 5, 6, {g: 7}],
+      b: new Date(),
+      c: /hello/g
+    }
+
+    const copy = deepCopy(original);
+
     expect(original).not.toBe(copy)
   })
 })
